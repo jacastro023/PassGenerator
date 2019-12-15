abf90f60336993b55d42c1ba6d36b6ff8242ff85
Jose Castro <jacastro023@gmail.com>
2019-12-14 20:41:11 -0600

    added arrays and randomizer to js file

--- script.js
+++ script.js
@@ -1,51 +1,46 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var copyBtn = document.querySelector("#copy")
+var content = document.querySelector("password")
 
 // options
-var option1 = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z];
-var option2 = [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z];
+var option1 = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var option2 = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var option3 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var option4 = [];
+var option4 = ["!", "$", "#", "%", "&", "[", "]", "(", ")", "*", "@", "`", "?", "<", ">"];
 
 
-// Write password to the #password input
-function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-
-    passwordText.value = password;
-
-    copyBtn.removeAttribute("disabled");
-    copyBtn.focus();
-}
-
-function copyToClipboard() {
-    // BONUS 
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-// BONUS EVENT LISTENER
-
-
-
-var option1 = ['a', 'b', 'c'];
-var option2 = ['D', 'E', 'F'];
-var option3 = ['1', '2', '3'];
-
+// setting prompt variables
+var userinput;
 var isOp1;
 var isOp2;
 var isOp3;
+var isOp4;
 var possibleOptions = [];
 
+// asking user what it wants
 function confirms() {
-    isOp1 = confirm('Would you like to enable option 1?');
-    isOp2 = confirm('Would you like to enable option 2?');
-    isOp3 = confirm('Would you like to enable option 3?');
+    userinput = prompt("how many characters would you like to use?")
+    isOp1 = confirm('Would you like to use lowercase letters?');
+    isOp2 = confirm('Would you like to use uppercase letters?');
+    isOp3 = confirm('Would you like to use numbers?');
+    isOp4 = confirm('Would you like to use special characters?');
     optionBuilder();
 };
 
+
+// Write password to the #password input
+// function writePassword() {
+//     var password = generatePassword();
+//     var passwordText = document.querySelector("#password");
+
+//     passwordText.value = password;
+
+//     copyBtn.removeAttribute("disabled");
+//     copyBtn.focus();
+// }
+
+// linking all of the options to be used
 function optionBuilder() {
     if (isOp1 || isOp2 || isOp3) {
         if (isOp1) {
@@ -59,24 +54,55 @@ function optionBuilder() {
         if (isOp3) {
             possibleOptions = possibleOptions.concat(option3)
         };
+        if (isOp4) {
+            possibleOptions = possibleOptions.concat(option4)
+        };
         getString();
     } else {
-        alert("You must enable an option");
+        alert("You must make a selection an option");
         confirms();
     }
 };
 
-function randomNumber() {
-    return Math.floor(Math.random() * possibleOptions.length)
+// adding to textarea
+function myFunction() {
+    document.getElementById("password").innerHTML = optionBuilder().value;
 };
 
+// adding amount of characters user selects
 function getString() {
     var text = "";
 
-    for (var i = 0; i < 4; i++) {
+    for (var i = 0; i < userinput; i++) {
         text += possibleOptions[randomNumber()]
     };
     alert(text);
 };
 
-confirms();
+// randomizing characters
+function randomNumber() {
+    return Math.floor(Math.random() * possibleOptions.length)
+};
+
+// adding the copy
+function copying() {
+    var copyText = document.getElementById("password");
+    copyText.select();
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+
+    // alert that it has been copies
+    alert("Copied the password: " + copyText.value);
+}
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", confirms); {
+
+}
+copyBtn.addEventListener("click", copying); {
+
+}
+
+
+// BONUS EVENT LISTENER
